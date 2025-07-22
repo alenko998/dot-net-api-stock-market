d5c0a3ee24c807974e8ef3edcdbcf62bcf28883d
alenko998 <alen_git@hotmail.com>
2025-07-22 20:07:09 +0200

    comment system

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -31,7 +31,7 @@ namespace api.Controllers
             var stocks = await _stockRepo.GetAllAsync();
             var stockDto = stocks.Select(s => s.ToStockDto());
 
-            return Ok(stocks);
+            return Ok(stockDto);
         }
 
         [HttpGet("{id}")]
