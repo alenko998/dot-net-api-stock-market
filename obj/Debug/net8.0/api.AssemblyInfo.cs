4362a45e7605322dff2025e1f92323ccb400affe
alenko998 <alen_git@hotmail.com>
2025-07-24 18:30:30 +0200

    Portfolio DELETE

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0f62aa66c97916cd62ed93857d96ed3a8f8a168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bca6cb3fc229551c4c738b75e23cf95c6332d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
