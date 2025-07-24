f013452989df9fac5ec3f8845fc0e6aa11f131f1
alenko998 <alen_git@hotmail.com>
2025-07-24 17:31:37 +0200

    Login

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b772abb1c70ccf329e89e24cabef6dca75152c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df46f85f115a2309e7e1fc90332f8ec63b92de85")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
