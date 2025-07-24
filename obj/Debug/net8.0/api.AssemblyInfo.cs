d0f62aa66c97916cd62ed93857d96ed3a8f8a168
alenko998 <alen_git@hotmail.com>
2025-07-24 18:05:57 +0200

    Portfolio GET

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f013452989df9fac5ec3f8845fc0e6aa11f131f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0605a96770a90ce7f85654d6183ed89e29436b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
